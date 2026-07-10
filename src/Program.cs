75550a222987d5574dc17835705b54835e66aed6
iminet <noreply@iminet.hu>
2026-07-10 20:33:58 +0200

    Restructured folders

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using Statiq.App;
+using Statiq.Web;
+
+namespace IminetSite
+{
+  public class Program
+  {
+    public static async Task<int> Main(string[] args) =>
+      await Bootstrapper
+        .Factory
+        .CreateWeb(args)
+        .RunAsync();
+  }
+}
